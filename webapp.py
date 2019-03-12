08625a7c66bdca5c8bad42296f6ff4891a3e359d
Jaryd <jarydchandos.5@gmail.com>
2019-03-12 11:12:54 -0700

    J

--- webapp.py
+++ webapp.py
@@ -12,3 +12,45 @@ app.debug = True #Change this to False for production
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
+@app.route('/', methods=["POST","GET"])
+def renderStore():
+    if "itemsincart" not in session or "clear" in request.args:
+        session["itemsincart"] = 0
+        print("skrt")
+        session["item_a"] = 0
+        session["item_b"] = 0
+        session["item_c"] = 0
+
+
+    return render_template("home.html",itemsincart=session["itemsincart"])
+
+
+@app.route('/Page1',methods=["POST","GET"])
+def renderAddToCart():
+    session["itemsincart"] += 1
+    if request.args["item"]=="a":
+        session["item_a"] += 1
+
+    elif request.args["item"]=="b":
+        session["item_b"] += 1
+
+    elif request.args["item"]=="c":
+        session["item_c"] += 1
+    return render_template("addToCart.html", item_name = "Item " + str(request.args["item"]))
+
+@app.route('/Page2',methods=["POST","GET"])
+def renderCheckout():
+    if "itemsincart" not in session:
+        return redirect(url_for("renderStore"))
+    return render_template("checkout.html",itemsincart=session["itemsincart"])
+
+@app.route('/Page3',methods=["POST","GET"])
+def renderBought():
+    if "itemsincart" not in session:
+        return redirect(url_for("renderStore"))
+    items = session["itemsincart"]
+    session["itemsincart"] = 0
+    return render_template("bought.html",itemsincart=str(items))
+
+if __name__=="__main__":
+    app.run(debug=True)
